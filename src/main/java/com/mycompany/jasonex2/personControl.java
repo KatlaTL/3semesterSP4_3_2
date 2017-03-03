861695f62099682d2bc03a4ab24461cf654b0ce3
Asger <regsa13@gmail.com>
2017-03-03 14:00:17 +0100

    first commit

---
+++ src/main/java/com/mycompany/jasonex2/personControl.java
@@ -0,0 +1,93 @@
+package com.mycompany.jasonex2;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Asger
+ */
+@WebServlet(name = "personControl", urlPatterns = {"/personControl"})
+public class personControl extends HttpServlet {
+
+    Gson gson = new Gson();
+    private static final List<Person> persons = new ArrayList<Person>() {
+        {
+            add(new Person("Fleming", "Hansen", 23));
+            add(new Person("Hans", "Jensen", 26));
+            add(new Person("Peter", "Petersen", 25));
+            add(new Person("Bob", "Hansen", 29));
+        }
+    };
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String jsonList = gson.toJson(persons, List.class);
+            out.println(jsonList);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getReader().lines().forEach((x) -> {
+            Person p = gson.fromJson(x, Person.class);
+            persons.add(p);
+        });
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
